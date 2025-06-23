0808e46311fe29eb3c3ccd0f512e1717987a6499
WildHonestFur <137202612+WildHonestFur@users.noreply.github.com>
2025-06-23 18:27:21 +0530

    Add files via upload

---
+++ Setup Reset.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='iamtheboss', host='127.0.0.1', autocommit=True)
+
+cur = cnx.cursor()
+
+cur.execute('CREATE DATABASE IF NOT EXISTS copterroyale;')
+cur.execute('USE copterroyale;')
+cur.execute('CREATE TABLE IF NOT EXISTS game(mode varchar(100));')
+cur.execute('SELECT COUNT(*) FROM game');
+if cur.fetchone()[0] < 1:
+    cur.execute("INSERT INTO game VALUES ('off')")
+cur.execute('CREATE TABLE IF NOT EXISTS users(username varchar(20), password varchar(20));')
+cur.execute('CREATE TABLE IF NOT EXISTS player(user varchar(20), name varchar(20), r int, g int, b int);')
+cur.execute('CREATE TABLE IF NOT EXISTS status(user varchar(20), state char);')
+cur.execute('CREATE TABLE IF NOT EXISTS stats(user varchar(20), games int, won int, kills int, maxkills int, topthree int);')
+
+cur.execute("UPDATE game SET mode = 'off';")
+cur.execute("UPDATE status SET state = 'i';")
